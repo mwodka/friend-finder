48b07f7a4a32fe45d552571f03aef189866c489d
mwodka <mrw.simple@gmail.com>
2016-10-31 11:54:44 -0500

    Update html-routes.js

--- html-routes.js
+++ html-routes.js
@@ -1,9 +1,16 @@
 var express = require('express');
-var app = express();
 
-app.get('/', function(req, res) {
-  res.send('public/home.html');
-});
-app.get('/survey', function(req, res) {
-  res.send('public/survey.html');
-});
+module.exports = (function() {
+    'use strict';
+    var router = express.Router();
+
+    router.get('/', function(req, res) {
+        res.render('index');
+    });
+
+    router.get('/survey', function (req, res) {
+        res.render('public/survey.html');
+    });
+
+    return router;    
+})();
