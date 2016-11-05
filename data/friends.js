88e9ee2aabff1fe1d72f1a617315c50889eed203
mwodka <mrw.simple@gmail.com>
2016-11-05 00:06:34 -0500

    added some friends to friendsArray for varied results in the survey

--- data/friends.js
+++ data/friends.js
@@ -5,17 +5,17 @@
 		"scores":[
 			5,
 			1,
-			4,
-			4,
 			5,
 			1,
-			2,
 			5,
-			4,
-			1
+			3,
+			5,
+			1,
+			1,
+			3
 		]
  	}, {
- 		"name": "Zoro",
+ 		"name": "Zorro",
 		"photo": "http://www.dogbreedinfo.com/images26/AmericanEskimoPurebredDogChloe3.jpg",
 		"scores": [
 			1,
@@ -29,7 +29,39 @@
 			5,
 			2
 		]
+ 	}, {
+ 		"name": "Jennanne",
+		"photo": "https://a2-images.myspacecdn.com/images01/29/64ab96b1cda4064008776a59e306fc95/300x300.jpg",
+		"scores": [
+			5,
+			5,
+			5,
+			5,
+			5,
+			5,
+			5,
+			5,
+			5,
+			5
+		]
+ 	}, {
+ 		"name": "Stuart",
+		"photo": "https://lh3.googleusercontent.com/-AI4jBSu1V8A/AAAAAAAAAAI/AAAAAAAAAAA/aoJWvmDW-N0/photo.jpg",
+		"scores": [
+			3,
+			3,
+			3,
+			3,
+			3,
+			3,
+			3,
+			3,
+			3,
+			3
+		]
  	}
  ];
 
  module.exports = friendsArray; 
+
+ 
